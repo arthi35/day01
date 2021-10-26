62754c419b540c47789f6ba346f91f6441a1514b
arthi35 <arthimba35@gmail.com>
2021-10-26 00:48:47 -0700

    First commit

---
+++ script2.js
@@ -0,0 +1,21 @@
+//step1: create an XHR object
+// variable name request should be used in whole program
+var request=new XMLHttpRequest();
+// step 2:create a connection
+//true:optional
+request.open('GET','https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json',true);
+//step3:send a request to kickstart request from client server (push)initiating path
+request.send();
+//step4:once data successfully received from server
+// http://200 server will assume through numbers servers data 
+// to client will be string so only we arer converting to another datatype onload is an event
+request.onload=function(){
+    // conversion from string to array of json object
+    var data=JSON.parse(request.response);
+    console.log(data);
+    // console.log(data[249].region);
+    for(i=0;i<data.length;i++){
+        console.log(data[i].id,data[i].name,data[i].email);
+      
+    }
+}
